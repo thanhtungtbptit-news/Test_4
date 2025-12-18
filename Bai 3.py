d3febce15eddeaa45bf374866cd756af0e85951c
ThanhTung <thanhtungtb.ptit@gmail.com>
2025-12-18 10:02:18 +0700

    Refactor Bai 3.py by removing duplicates
    Removed duplicate functions and print statements.

--- Bai 3.py
+++ Bai 3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def boolen(x,list):
     for i in x:
         if i == list:
@@ -12,18 +11,4 @@ def creat(list):
         kq.add(i)
     return kq
 print(creat([1,2,3,4,5,2]))
-=======
-def boolen(x,list):
-    for i in x:
-        if i == list:
-            return True
-    return False
-print(boolen([1,2,3],3))
 
-def creat(list):
-    kq=set()
-    for i in list:
-        kq.add(i)
-    return kq
-print(creat([1,2,3,4,5,2]))
->>>>>>> 96da685e41dbe14a279e3fab5fdfef40879fb929
